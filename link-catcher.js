b26f9003dce76b43ec8a4e2455c819f582b04eb8
Mat Johnson <mat@demotive.com>
2013-11-06 13:02:44 +0000

    Oops. Adding the js I actually built.

--- link-catcher.js
+++ link-catcher.js
@@ -1,11 +1,21 @@
-// Catch any links out to https://gov.uk and kill them
+// No idea why the logo needs hacking, probably my lame indexOf stuff below
+var logo = document.getElementById('logo');
+logo.onclick = function() {
+  return false;
+};
 
-alert("yep");
-
-var els = document.getElementsByTagName('a');
-
-for (var i=0; i<els.length; i++) {
-  console.log(this.getAttribute('href'));
-}
-
-// Within "Limelight", handle any links clicked WITHOUT the .html suffix
+document.onclick = function(event) {
+    var el = event.target;
+    if (el.nodeName == "A") {
+      var link = el.getAttribute('href');
+      // Catch any links out to https://gov.uk (etc) and kill them
+      if (link.indexOf('https://www.gov.uk/') != -1 || link.indexOf('https://www.surveymonkey.com/') != -1 || link.indexOf('http://digital.cabinetoffice.gov.uk/') != -1 || link.indexOf('http://www.nationalarchives.gov.uk/') != -1) {
+        return false;
+      }
+      // Within "Limelight", handle any links clicked WITHOUT the .html suffix
+      if (link.indexOf('.html') == -1) {
+        window.location.href = link + '.html';
+        return false;
+      }
+    }
+};
